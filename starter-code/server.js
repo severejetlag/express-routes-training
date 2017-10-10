43c1a79a7a70a0c7caf66d02aaec02ef5258ed47
severejetlag <nhl1013@gmail.com>
2017-10-10 09:22:34 -0700

    completed as much of the assigment as possible without POST

--- starter-code/server.js
+++ starter-code/server.js
@@ -3,10 +3,28 @@
 
 var express = require('express');
 var app = express();
-
+var guessNum = 8; 
+var artworks = [
+	{
+		title: 'Mona Lisa',
+		artist: 'Da Vinci',
+		desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	},
+	{
+		title: 'The Starry Night',
+		artist: 'Vincent van Gogh',
+		desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	},
+	{
+		title: 'Girl with a Pearl Earing',
+		artist: 'Johannes Vermeer.',
+		desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	}
+];
 
 // MIDDLEWARE
 app.use(express.static('public'));
+
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,24 +32,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Allow CORS:
 // not necessary since we'll be making requests from a js file
   // that we are also serving (as static assets in public)
-// app.use(function(request, response, next) {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // ROUTES
 // Root Route
+app.get('/',function(req,res){
+	res.sendFile('views/index.html' , { root : __dirname});
+})
 
 
 // Gallery View Route
 
 
 // The Number Guessing Game
-
+app.get('/number', function(req,res){
+	console.log(req.query);
+  	var num = req.query.num; 
+  	if(num === guessNum){
+	  	res.send(`You guessed correct!`);
+  	}else if(num > guessNum){
+  		res.send("Too high!");
+  	}else if(num < guessNum){
+  		res.send("Too low!");
+  	}
+})
 
 // Gallery
-
+app.get('/api/gallery', function(req,res){
+	res.json(artworks);
+})
 
 // SERVER START
 var port = 3000;
